ad3f45f200865a25b1bacb5cb46fd3482a5e0178
Radon Rosborough <radon.neon@gmail.com>
2022-06-04 15:26:40 -0700

    Update

--- venmo_auto_transfer.py
+++ venmo_auto_transfer.py
@@ -9,9 +9,6 @@ import dotenv
 import requests
 
 
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"
-
-
 def log(msg):
     print(msg, file=sys.stderr)
 
@@ -21,44 +18,50 @@ def fatal(msg):
     sys.exit(1)
 
 
-def login(username, password):
-    device_id = f"fp01-{uuid.uuid4()}"
-    resp = requests.post(
-        "https://venmo.com/login",
-        data={
-            "phoneEmailUsername": username,
-            "password": password,
-            "return_json": "true",
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"
+
+
+def get_current_balance(access_token):
+    resp = requests.get(
+        "https://account.venmo.com/api/user/identities",
+        cookies={
+            # If v_id is omitted the request is blocked with a 404, but
+            # the value does not seem to be checked. Normally it is a
+            # device ID in the form of a UUID with the prefix fp01, but I
+            # have decided to ignore that since it does not seem to be a
+            # requirement.
+            "v_id": "null",
+            "api_access_token": access_token,
         },
+        # Setting the user agent is not required per se, but the
+        # requests library user agent appears to be blocked
+        # specifically, so we have to set it to something.
         headers={
-            "Cookie": f"v_id={device_id}",
             "User-Agent": user_agent,
         },
     )
-    if resp.status_code != 401:
-        log(resp.text)
-        fatal(f"got unexpected status code {resp.status_code} on /login POST")
-    try:
-        if resp.json()["error"]["message"] != "Additional authentication is required":
-            raise Exception
-    except Exception:
-        log(resp.text)
-        fatal(f"got unexpected response from /login POST")
-    otp_secret = resp.headers["venmo-otp-secret"]
-    print(otp_secret)
-
-
-def get_and_print_status(username, password):
-    login(username, password)
+    assert resp.status_code == 200, resp.status_code
+    personal_account = [
+        acct for acct in resp.json() if acct["identityType"] == "personal"
+    ][0]
+    return personal_account["balance"]
 
 
 def main():
     parser = argparse.ArgumentParser("venmo_auto_transfer")
-    parser.parse_args()
+    parser.add_argument(
+        "-t",
+        "--transfer",
+        action="store_true",
+        help="Transfer balance to bank account if nonzero",
+    )
+    args = parser.parse_args()
     dotenv.load_dotenv()
-    username = os.environ["VENMO_USERNAME"]
-    password = os.environ["VENMO_PASSWORD"]
-    get_and_print_status(username, password)
+    access_token = os.environ["VENMO_TOKEN"]
+    balance = get_current_balance(access_token)
+    log(f"current Venmo balance is ${balance:.2f}")
+    if args.transfer and balance > 0:
+        log("transferring to linked bank account")
     sys.exit(0)
 
 
