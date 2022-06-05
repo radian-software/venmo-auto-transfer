bf9af2403a9e856980199ed19fae6c11d4e0052c
Radon Rosborough <radon.neon@gmail.com>
2022-06-05 14:29:39 -0700

    Finish login flow and balance transfer

--- venmo_auto_transfer.py
+++ venmo_auto_transfer.py
@@ -6,6 +6,7 @@ import json
 import os
 import re
 import sys
+import types
 import uuid
 
 import dotenv
@@ -23,6 +24,19 @@ def fatal(msg):
 
 user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"
 device_id = f"fp01-{uuid.uuid4()}"
+
+
+def get_csrf_data(resp):
+    assert resp.status_code == 200, resp.status_code
+    csrf_cookie = resp.cookies["_csrf"]
+    next_data_match = re.search(
+        r'<script id="__NEXT_DATA__" type="application/json">([^<>]+)</script>',
+        resp.text,
+    )
+    assert next_data_match
+    next_data = json.loads(next_data_match.group(1))
+    csrf_token = next_data["props"]["pageProps"]["csrfToken"]
+    return types.SimpleNamespace(cookie=csrf_cookie, token=csrf_token)
 
 
 def perform_login(username, password, bank_account_number):
@@ -50,36 +64,29 @@ def perform_login(username, password, bank_account_number):
         log(resp.text)
         fatal(f"got unexpected response from /login POST")
     otp_secret = resp.headers["venmo-otp-secret"]
-    resp = requests.get(
-        "https://account.venmo.com/account/mfa/verify-bank",
-        params={
-            "k": otp_secret,
-        },
-        cookies={
-            "v_id": device_id,
-        },
-        headers={
-            "user-agent": user_agent,
-        },
+    csrf = get_csrf_data(
+        requests.get(
+            "https://account.venmo.com/account/mfa/verify-bank",
+            params={
+                "k": otp_secret,
+            },
+            cookies={
+                "v_id": device_id,
+            },
+            headers={
+                "user-agent": user_agent,
+            },
+        )
     )
-    assert resp.status_code == 200, resp.status_code
-    csrf_cookie = resp.cookies["_csrf"]
-    next_data_match = re.search(
-        r'<script id="__NEXT_DATA__" type="application/json">([^<>]+)</script>',
-        resp.text,
-    )
-    assert next_data_match
-    next_data = json.loads(next_data_match.group(1))
-    csrf_token = next_data["props"]["pageProps"]["csrfToken"]
     resp = requests.post(
         "https://account.venmo.com/api/account/mfa/sign-in",
         cookies={
             "v_id": device_id,
-            "_csrf": csrf_cookie,
+            "_csrf": csrf.cookie,
         },
         headers={
-            "csrf-token": csrf_token,
-            "xsrf-token": csrf_token,
+            "csrf-token": csrf.token,
+            "xsrf-token": csrf.token,
             "venmo-otp-secret": otp_secret,
             "user-agent": user_agent,
         },
@@ -88,7 +95,8 @@ def perform_login(username, password, bank_account_number):
         },
     )
     assert resp.status_code == 200, resp.status_code
-    return resp.cookies["api_access_token"]
+    access_token = resp.cookies["api_access_token"]
+    return access_token
 
 
 def get_current_balance(access_token):
@@ -113,13 +121,24 @@ def get_current_balance(access_token):
 
 
 def get_primary_bank_id(access_token):
-    csrf_cookie = NotImplemented
+    csrf = get_csrf_data(
+        requests.get(
+            "https://account.venmo.com/cashout",
+            cookies={
+                "v_id": device_id,
+                "api_access_token": access_token,
+            },
+            headers={
+                "user-agent": user_agent,
+            },
+        )
+    )
     resp = requests.get(
         "https://account.venmo.com/api/payment-methods",
         cookies={
             "v_id": device_id,
             "api_access_token": access_token,
-            "_csrf": csrf_cookie,
+            "_csrf": csrf.cookie,
         },
         headers={
             "user-agent": user_agent,
@@ -127,21 +146,32 @@ def get_primary_bank_id(access_token):
     )
     assert resp.status_code == 200, resp.status_code
     primary_bank = [
-        bank for bank in resp.json() if bank["roles"]["balanceTransfers"] == "primary"
+        bank
+        for bank in resp.json()
+        if bank["roles"].get("balanceTransfers") == "primary"
     ][0]
     return primary_bank["value"]
 
 
-def transfer_balance(access_token, amount):
-    csrf_cookie = NotImplemented
-    csrf_token = NotImplemented
-    bank_id = NotImplemented
+def transfer_balance(access_token, bank_id, amount):
+    csrf = get_csrf_data(
+        requests.get(
+            "https://account.venmo.com/cashout",
+            cookies={
+                "v_id": device_id,
+                "api_access_token": access_token,
+            },
+            headers={
+                "user-agent": user_agent,
+            },
+        )
+    )
     resp = requests.post(
         "https://account.venmo.com/api/transfer",
         cookies={
             "v_id": device_id,
             "api_access_token": access_token,
-            "_csrf": csrf_cookie,
+            "_csrf": csrf.cookie,
         },
         json={
             "fundingInstrumentId": bank_id,
@@ -150,7 +180,7 @@ def transfer_balance(access_token, amount):
         },
         headers={
             "user-agent": user_agent,
-            "csrf-token": csrf_token,
+            "csrf-token": csrf.token,
         },
     )
     assert resp.status_code == 201, resp.status_code
@@ -166,15 +196,29 @@ def main():
     )
     args = parser.parse_args()
     dotenv.load_dotenv()
-    username = os.environ["VENMO_USERNAME"]
-    password = os.environ["VENMO_PASSWORD"]
-    bank_account_number = os.environ["VENMO_BANK_ACCOUNT_NUMBER"]
-    access_token = perform_login(username, password, bank_account_number)
+    access_token = os.environ.get("VENMO_ACCESS_TOKEN")
+    if access_token:
+        log(f"using existing API access token from .env (not validated)")
+    else:
+        username = os.environ["VENMO_USERNAME"]
+        log(
+            f"attempting to login to Venmo as {username} with password and bank account number"
+        )
+        access_token = perform_login(
+            username,
+            os.environ["VENMO_PASSWORD"],
+            os.environ["VENMO_BANK_ACCOUNT_NUMBER"],
+        )
+        log(f"obtained new API access token from login")
     balance = get_current_balance(access_token)
     log(f"current Venmo balance is ${balance:.2f}")
     if args.transfer and balance > 0:
-        primary_bank_id = get_primary_bank_id(access_token)
-        log(f"primary bank account has ID ${primary_bank_id}")
+        bank_id = get_primary_bank_id(access_token)
+        amount = decimal.Decimal("0.01")
+        log(f"transferring ${amount:.2f} to primary bank account with ID {bank_id}")
+        transfer_balance(access_token, bank_id, amount)
+        new_balance = get_current_balance(access_token)
+        log(f"success; new balance is ${new_balance:.2f}")
     sys.exit(0)
 
 
